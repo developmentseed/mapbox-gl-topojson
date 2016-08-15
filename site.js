90de74650e6d3e98cc5d4e6376347c3cd763494c
Anand Thakker <vestibule@anandthakker.net>
2016-08-15 18:48:16 -0400

    Use sourceTypes: option

--- site.js
+++ site.js
@@ -2,13 +2,22 @@ var mapboxgl = require('mapbox-gl')
 
 mapboxgl.accessToken = getAccessToken()
 
-mapboxgl.Source.addType('topojson', require('./topojson_source'))
+// Alternative to the `sourceTypes:` option is to do the following
+// before creating the map:
+//
+// mapboxgl.Source.addType('topojson', require('./topojson_source'))
+//
+// (This can also be done after, but that requires listening for the
+// 'source-type.add' event before using it.)
 
 var map = window.map = new mapboxgl.Map({
   container: 'map',
   zoom: 5.2,
   center: [-119.393, 36.883],
-  style: 'mapbox://styles/mapbox/streets-v8'
+  style: 'mapbox://styles/mapbox/streets-v8',
+  sourceTypes: {
+    topojson: require('./topojson_source')
+  }
 })
 
 map.addControl(new mapboxgl.Navigation())
