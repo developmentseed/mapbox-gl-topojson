eb67d58aea4fd6364c2c65a4a80d575bba9b7591
Anand Thakker <vestibule@anandthakker.net>
2016-08-15 18:44:09 -0400

    Update live version

--- dist/bundle.js
+++ dist/bundle.js
@@ -30058,74 +30058,89 @@ Object.defineProperty(exports, '__esModule', { value: true });
 }));
 },{}],203:[function(require,module,exports){
 var mapboxgl = require('mapbox-gl')
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJjaW4wOW1taTkwN3c5d2tsdXg3aXRrMjZhIn0.QpBLQwr3XzYs6xh6b8hbcQ'
+
+mapboxgl.accessToken = getAccessToken()
 
 mapboxgl.Source.addType('topojson', require('./topojson_source'))
 
 var map = window.map = new mapboxgl.Map({
-    container: 'map',
-    zoom: 5.2,
-    center: [-119.393, 36.883],
-    style: 'mapbox://styles/mapbox/streets-v8'
-});
+  container: 'map',
+  zoom: 5.2,
+  center: [-119.393, 36.883],
+  style: 'mapbox://styles/mapbox/streets-v8'
+})
 
-map.addControl(new mapboxgl.Navigation());
+map.addControl(new mapboxgl.Navigation())
 
-map.on('load', function() {
+map.on('load', function () {
   map.addSource('counties', {
-      type: 'topojson',
-      data: 'ca.json',
-      workerOptions: {
-        layer: 'counties'
-      }
+    type: 'topojson',
+    data: 'ca.json',
+    workerOptions: {
+      layer: 'counties'
+    }
   })
 
   map.addLayer({
-      "id": "county-boundaries",
-      "type": "line",
-      "source": "counties",
-      "paint": {
-          "line-color": "#EC8D8D",
-          "line-width": {
-              "base": 1.5,
-              "stops": [
-                  [
-                      5,
-                      0.75
-                  ],
-                  [
-                      18,
-                      32
-                  ]
-              ]
-          }
+    'id': 'county-boundaries',
+    'type': 'line',
+    'source': 'counties',
+    'paint': {
+      'line-color': '#EC8D8D',
+      'line-width': {
+        'base': 1.5,
+        'stops': [
+          [
+            5,
+            0.75
+          ],
+          [
+            18,
+            32
+          ]
+        ]
       }
-  }, 'country-label-lg');
+    }
+  }, 'country-label-lg')
 })
+
+function getAccessToken () {
+  var accessToken = (
+        getURLParameter('access_token') ||
+        localStorage.getItem('accessToken')
+    )
+  localStorage.setItem('accessToken', accessToken)
+  return accessToken
+}
+
+function getURLParameter (name) {
+  var regexp = new RegExp('[?&]' + name + '=([^&#]*)', 'i')
+  var output = regexp.exec(window.location.href)
+  return output && output[1]
+}
 
 },{"./topojson_source":204,"mapbox-gl":82}],204:[function(require,module,exports){
 var util = require('mapbox-gl/js/util/util')
 var GeoJSONSource = require('mapbox-gl/js/source/geojson_source')
 var webworkify = require('webworkify')
 
-module.exports = TopoJSONSource;
+module.exports = TopoJSONSource
 
 function TopoJSONSource (id, options, dispatcher) {
   GeoJSONSource.call(this, id, options, dispatcher)
 }
 
 TopoJSONSource.prototype = util.inherit(GeoJSONSource, {
-    type: 'topojson'
+  type: 'topojson'
 })
 
-TopoJSONSource.workerSourceURL = URL.createObjectURL(webworkify(require('./topojson_worker.js'), {bare: true}));
+TopoJSONSource.workerSourceURL = URL.createObjectURL(webworkify(require('./topojson_worker.js'), {bare: true}))
 
 
 },{"./topojson_worker.js":205,"mapbox-gl/js/source/geojson_source":97,"mapbox-gl/js/util/util":181,"webworkify":200}],205:[function(require,module,exports){
 var util = require('mapbox-gl/js/util/util')
 var topojson = require('topojson')
 var GeoJSONWorkerSource = require('mapbox-gl/js/source/geojson_worker_source')
-var ajax = require('mapbox-gl/js/util/ajax')
 
 function loadGeoJSON (params, callback) {
   // defer to the default implementation to grab / JSON.parse the data, which
@@ -30133,21 +30148,20 @@ function loadGeoJSON (params, callback) {
   GeoJSONWorkerSource.prototype.loadGeoJSON.call(this, params, function (err, topo) {
     if (err) { return callback(err) }
     // once we have it, convert the layer specified in `params` to geojson.
-    var data =  topojson.feature(topo, topo.objects[params.layer])
+    var data = topojson.feature(topo, topo.objects[params.layer])
     console.log('data', data, params, topo)
     callback(null, data)
   })
 }
 
 function TopoJSONWorkerSource (actor, style) {
-  debugger
-  GeoJSONWorkerSource.call(this, actor, style, loadGeoJSON);
+  GeoJSONWorkerSource.call(this, actor, style, loadGeoJSON)
 }
 
-TopoJSONWorkerSource.prototype = util.inherit(GeoJSONWorkerSource);
+TopoJSONWorkerSource.prototype = util.inherit(GeoJSONWorkerSource)
 
 module.exports = function (self) {
-  self.registerWorkerSource('topojson', TopoJSONWorkerSource);
-};
+  self.registerWorkerSource('topojson', TopoJSONWorkerSource)
+}
 
-},{"mapbox-gl/js/source/geojson_worker_source":98,"mapbox-gl/js/util/ajax":164,"mapbox-gl/js/util/util":181,"topojson":191}]},{},[203]);
+},{"mapbox-gl/js/source/geojson_worker_source":98,"mapbox-gl/js/util/util":181,"topojson":191}]},{},[203]);
